61f8f886785e6708cbb6e866126083f4452f263d
agit15 <34344946+agit15@users.noreply.github.com>
2019-10-18 11:15:43 +0200

    genre-service en discovery-server

---
+++ genre-service/src/main/java/be/thomasmore/genreservice/GenreServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.genreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenreServiceApplication.class, args);
+	}
+
+}
