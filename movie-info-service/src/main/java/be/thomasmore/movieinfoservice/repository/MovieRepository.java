6574328e9241636be32ec698e0e05427cfa289e4
wesleyjanse <wesleyjanse@gmail.com>
2019-10-18 11:08:28 +0200

    movie-info-service

--- movie-info-service/src/main/java/be/thomasmore/movieinfoservice/repository/MovieRepository.java
+++ movie-info-service/src/main/java/be/thomasmore/movieinfoservice/repository/MovieRepository.java
@@ -1,5 +1,12 @@
 package be.thomasmore.movieinfoservice.repository;
 
-public interface MovieRepository {
-    
+
+import be.thomasmore.movieinfoservice.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    Movie findMovieByTitle(@Param("title") String title);
 }
