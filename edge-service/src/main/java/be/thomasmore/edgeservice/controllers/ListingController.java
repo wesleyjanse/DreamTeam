b2ced1a6dde075e6df72f09393253faeb84e96f5
brecht snoeck <r0621517@student.thomasmore.be>
2019-10-25 11:06:56 +0200

    dream-teams-service

--- edge-service/src/main/java/be/thomasmore/edgeservice/controllers/ListingController.java
+++ edge-service/src/main/java/be/thomasmore/edgeservice/controllers/ListingController.java
@@ -26,90 +26,16 @@ public class ListingController {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @GetMapping("user/{userId}")
-    public List<ListingItem> getListingItemsByUserId(@PathVariable("userId") Integer userId) {
+    @GetMapping("/all")
+    public List<DreamTeam> getAllDreamTeams() {
+        GenericResponseWrapper wrapper = restTemplate.getForObject("http://dream-teams-service/dreamTeams", GenericResponseWrapper.class);
 
-        GenericResponseWrapper wrapper = restTemplate.getForObject("http://rating-service/ratings/search/findRatingsByUserId?userid=" + userId, GenericResponseWrapper.class);
+        List<DreamTeam> dreamTeams = objectMapper.convertValue(wrapper.get_embedded().get("dreamTeams"), new TypeReference<List<DreamTeam>>() { });
 
-        List<Rating> ratings = objectMapper.convertValue(wrapper.get_embedded().get("ratings"), new TypeReference<List<Rating>>() { });
-
-        List<ListingItem> returnList = new ArrayList<>();
-        for (Rating rating: ratings) {
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-            returnList.add(new ListingItem(movie.getTitle(), rating.getScoreNumber()));
-        }
-        return returnList;
+        return dreamTeams;
     }
 
-    @GetMapping("movie/{title}")
-    public List<ListingItem> getListingItemsByMovieTitle(@PathVariable("title") String title){
 
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/search/findMovieByTitle?title=" + title, Movie.class);
 
-        GenericResponseWrapper wrapper = restTemplate.getForObject("http://rating-service/ratings/search/findRatingsByMovieId?movieid=" + movie.getId(), GenericResponseWrapper.class);
-
-        List<Rating> ratings = objectMapper.convertValue(wrapper.get_embedded().get("ratings"), new TypeReference<List<Rating>>() { });
-
-        List<ListingItem> returnList = new ArrayList<>();
-        for (Rating rating: ratings) {
-            returnList.add(new ListingItem(movie.getTitle(), rating.getScoreNumber(), rating.getUserId()));
-        }
-        return returnList;
-    }
-
-    @GetMapping("user/{userId}/movie/{title}")
-    public ListingItem getListingItemsByUserIdAndMovieTitle(@PathVariable("userId") Integer userId, @PathVariable("title") String title){
-
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/search/findMovieByTitle?title=" + title, Movie.class);
-
-        Rating rating = restTemplate.getForObject("http://rating-service/ratings/search/findRatingByUserIdAndMovieId?userid=" + userId + "&movieid=" + movie.getId(), Rating.class);
-
-        ListingItem listingItem = new ListingItem(movie.getTitle(), rating.getScoreNumber());
-        return listingItem;
-
-    }
-
-    @PostMapping("user/{userId}")
-    public ResponseEntity<String> postListingItemsByUserId(@PathVariable("userId") Integer userId, @RequestBody ListingItem listingItem){
-
-        List<HttpMessageConverter<?>> list = new ArrayList<>();
-        list.add(new MappingJackson2HttpMessageConverter());
-        restTemplate.setMessageConverters(list);
-
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/search/findMovieByTitle?title=" + listingItem.getTitle(), Movie.class);
-
-        Rating rating = new Rating(userId, movie.getId() , listingItem.getScore());
-
-        ResponseEntity<String> result = restTemplate.postForEntity("http://rating-service/ratings/", rating, String.class);
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("user/{userId}")
-    public ResponseEntity<String> putListingItemsByUserId(@PathVariable("userId") Integer userId, @RequestBody ListingItem listingItem){
-
-        List<HttpMessageConverter<?>> list = new ArrayList<>();
-        list.add(new MappingJackson2HttpMessageConverter());
-        restTemplate.setMessageConverters(list);
-
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/search/findMovieByTitle?title=" + listingItem.getTitle(), Movie.class);
-
-        Rating rating = restTemplate.getForObject("http://rating-service/ratings/search/findRatingByUserIdAndMovieId?userid=" + userId + "&movieid=" + movie.getId(), Rating.class);
-        rating.setScoreNumber(listingItem.getScore());
-
-        restTemplate.put("http://rating-service/ratings/" + rating.getId(), rating, String.class);
-        return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping("user/{userId}/movie/{title}")
-    public ResponseEntity deleteListingItemsByUserIdAndMovieTitle(@PathVariable("userId") Integer userId, @PathVariable("title") String title){
-
-        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/search/findMovieByTitle?title=" + title, Movie.class);
-
-        Rating rating = restTemplate.getForObject("http://rating-service/ratings/search/findRatingByUserIdAndMovieId?userid=" + userId + "&movieid=" + movie.getId(), Rating.class);
-
-        restTemplate.delete("http://rating-service/ratings/" + rating.getId());
-
-        return ResponseEntity.ok().build();
-    }
 
 }
