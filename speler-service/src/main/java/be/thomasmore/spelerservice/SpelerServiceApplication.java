cf50f575d3c63bee9a8f7de72ff6b00abd07bc91
wesleyjanse <wesleyjanse@gmail.com>
2019-10-25 10:58:23 +0200

    speler-service

---
+++ speler-service/src/main/java/be/thomasmore/spelerservice/SpelerServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.spelerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpelerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpelerServiceApplication.class, args);
+	}
+
+}
