57ecadfbacff70c89e6c5d46e1a66359090c9dab
wesleyjanse <wesleyjanse@gmail.com>
2019-11-08 10:40:27 +0100

    CSS -> SCSS

--- DreamTeamDbFrontEnd/src/app/app.component.ts
+++ DreamTeamDbFrontEnd/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {DreamTeamDbService} from "./dream-team-db.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'DreamTeamDbFrontEnd';
--- DreamTeamDbFrontEnd/src/app/app.module.ts
+++ DreamTeamDbFrontEnd/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShowtextComponent } from './showtext/showtext.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowtextComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
