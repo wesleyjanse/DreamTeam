6248059cab78d7e8c5d322434c6a01090d733495
wesleyjanse <wesleyjanse@gmail.com>
2019-11-15 08:49:26 +0100

    login frontend af

--- DreamTeamDbFrontEnd/src/app/app.module.ts
+++ DreamTeamDbFrontEnd/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './security/login/login.component';
+import { SecurityModule } from './security/security.module'
 
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
+  ];
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SecurityModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ DreamTeamDbFrontEnd/src/app/models/member.model.ts
@@ -0,0 +1,7 @@
+export class Member {
+    constructor(public memberID: number,
+        public username: string,
+        public password: string,
+        public token: string
+        ) { }
+}
